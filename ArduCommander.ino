34640afbde50705fd6581147fdb51ba92b92a077
Chayan Mistry <chayanforyou@users.noreply.github.com>
2016-03-13 10:53:42 +0600

    Added files via upload

---
+++ ArduCommander.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  String msg = "";
+
+  if (Serial.available() > 0) {
+    while (Serial.available() > 0) {
+      msg += char(Serial.read());
+      delay(100);
+    }
+    Serial.println(msg);
+  }
+}
